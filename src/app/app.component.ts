746a999829b6f3e2af83493c5b522a9b70e97a85
Khalil Tizaoui <62132130+TizaouiKha@users.noreply.github.com>
2024-04-23 12:07:26 +0200

    creation of login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {environment} from "../app/environnements/environnement";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'notation';
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(){
+    console.log(environment);
+    this.http.get(environment.apiUrl +"/student/all").subscribe({
+      next: res=>
+        console.log(res)
+    })
+  }
 }
